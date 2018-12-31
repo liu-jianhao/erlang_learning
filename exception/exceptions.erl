198c7756c826785a0d6dfe3f113e2563760b0d9b
liu-jianhao <jianhaoliu17@email.com>
2018-12-31 15:34:04 +0800

    exceptions

---
+++ exception/exceptions.erl
@@ -0,0 +1,79 @@
+-module(exceptions).
+-compile(export_all).
+
+%% 处理异常
+throws(F) ->
+    try F() of
+        _ -> ok
+    catch
+        Throw -> {throw, caught, Throw}
+    end.
+
+errors(F) ->
+    try F() of
+        _ -> ok
+    catch
+        error:Error -> {error, caught, Error}
+    end.
+
+exits(F) ->
+    try F() of
+        _ -> ok
+    catch
+        exit:Exit -> {exit, caught, Exit}
+    end.
+
+sword(1) -> throw(slice);
+sword(2) -> erlang:error(cut_arm);
+sword(3) -> exit(cut_leg);
+sword(4) -> throw(punch);
+sword(5) -> exit(cross_bridge).
+
+%% is_function()是一个BIF函数，保证Attack是一个参数为0的函数
+black_knight(Attack) when is_function(Attack, 0) ->
+    try Attack() of
+        _ -> "None shall pass."
+    catch
+        throw:slice -> "It is but a scratch.";
+        error:cut_arm -> "I've had worse.";
+        exit:cut_leg -> "Come on you pansy!";
+        _:_ -> "Just a flesh wound."
+    end.
+
+talk() -> "blah blah".
+
+%% try 和 of 之间可以有多个表达式
+whoa() ->
+    try
+        talk(),
+        _Knight = "None shall Pass!",
+        _Doubles = [N*2 || N <- lists:seq(1,100)],
+        throw(up),
+        _WillReturnThis = tequila
+    of
+        tequila -> "hey this worked!"
+    catch
+        Exception:Reason -> {caught, Exception, Reason}
+    end.
+
+%% 当有多个表达式时，of 部分就变得没什么用，可以直接去掉
+im_impressed() ->
+    try
+        talk(),
+        _Knight = "None shall Pass!",
+        _Doubles = [N*2 || N <- lists:seq(1,100)],
+        throw(up),
+        _WillReturnThis = tequila
+    catch
+        Exception:Reason -> {caught, Exception, Reason}
+    end.
+
+%% catch 关键字
+catcher(X,Y) ->
+    case catch X/Y of
+        {'EXIT', {badarith,_}} -> "uh oh";
+        N -> N
+    end.
+
+one_or_two(1) -> return;
+one_or_two(2) -> throw(return).
